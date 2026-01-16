2b76763d6082798e9718000ad332d967a943cb32
wazkayd <wazkayd.sbs@gmail.com>
2026-01-16 08:48:54 +0100

    Diesel-page/header-card fixes

--- src/components/DieselHeader.js
+++ src/components/DieselHeader.js
@@ -2,44 +2,48 @@ import { Spin } from "antd";
 import React from "react";
 
 const DieselHeader = ({ dieselPrice, genStatus, Co2, loader }) => {
+  
   return (
     <div className="dashboard-header">
       <div className="header-cards">
-        {/* Generator Info */}
-        <Spin spinning={loader.branchGeneratorsStatusLoading}>
+        {/* Generator Info */}       
           <div className="header-card generator-card">
             <div className="gen-left">
               <span className="card-icon">
                 <img src="/electric-generator_2695268 1.png" alt="Logo" />
               </span>
-            </div>
-            {genStatus?.generators?.map((gen, index) => (
-              <React.Fragment>
-                <div className="gen-center">
-                  <p className="card-label title">Generator</p>
-                </div>
-                <div className="gen-right">
-                  <p className="card-value">
-                    <span className={`status-dot ${gen.is_currently_on ? 'on' : ''}`}></span> {gen.name}
-                  </p>
-                  <p className="last-posted">
-                    Last used <span className="value-red">{gen.last_usage_time_relative}</span>
-                  </p>
-                </div>
-              </React.Fragment>
-            ))}
           </div>
-        </Spin>
+          <div className="">
+            <p className="card-label" style={{ fontSize: 20, marginLeft: 10 }}>
+              {genStatus ? genStatus?.generators?.map((gen, index) => (
+                <React.Fragment>
+                  <Spin spinning={loader.branchGeneratorsStatusLoading}>
+                    <div className="">
+                      <p className="">
+                        <span className={`status-dot ${gen.is_currently_on ? 'on' : ''}`}></span> {gen.name}
+                      </p>
+                      <p className="last-posted" style={{ fontSize: 14, marginLeft: 2 }}>
+                        Last used <span className="value-red">{gen.last_usage_time_relative}</span>
+                      </p>
+                    </div>
+                  </Spin>
+                </React.Fragment>
+              )) :
+                'Generator'
+              }
+            </p>
+          </div>
+        </div>
 
         {/* CO Emission */}
         <div className="header-card emission-card">
           <span className="card-icon">
-            <img src="/Vector.png" alt="Logo" />
+            <img width={60} src="/Vector.png" alt="Logo" />
           </span>
-          <div className="emission-info">
-            <p className="card-label">CO emission</p>
+          <div className="emission-info" style={{marginLeft:10}}>
+            <p className="card-label" style={{fontSize:20}}>CO emission</p>
             <Spin spinning={loader.co2EmissionLoading}>
-              <p className="card-value highlight">
+              <p className="card-value highlight" style={{fontSize:20}}>
                 {(Co2?.data?.total_co2_tonnes)?.toLocaleString()} <span className="unit">tons</span>
               </p>
             </Spin>
@@ -49,15 +53,15 @@ const DieselHeader = ({ dieselPrice, genStatus, Co2, loader }) => {
         {/* Price / Diesel Info */}
         
         <div className="header-card price-card">
-          <div className="price-left">
+          <div className="price-left" style={{marginRight:10}}>
             <span className="card-icon">
-              <img src="/i-gun.png" alt="Logo" />
+              <img width={40} src="/i-gun.png" alt="Logo" />
             </span>
           </div>
           <div className="price-center">
-            <p className="card-label">Price / Litre</p>
-            <p className="card-label">Diesel efficiency</p>
-            <p className="card-label">Cost (Estimated monthly)</p>
+            <p className="card-label-price">Price / Litre</p>
+            <p className="card-label-price">Diesel efficiency</p>
+            <p className="card-label-price">Cost (Estimated monthly)</p>
           </div>
           <Spin spinning={loader.dieselPriceLoading}>
             <div className="price-right">
